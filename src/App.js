1b56dacc515dbc7e98e3fd10d18e4edd005ea705
Robarto <robartpetrov@gmail.com>
2022-12-25 19:10:08 +0200

    Finished logic and mobile styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./styles.scss";
+import logo from "./images/logo.svg";
+import document from "./images/icon-document.svg";
+import folder from "./images/icon-folder.svg";
+import upload from "./images/icon-upload.svg";
+import { useState } from "react";
 function App() {
+  const [usedStorage, setUsedStorage] = useState(815);
+
+  const handleChange = (e) => {
+    setUsedStorage(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <main>
+      <div className="info-container">
+        <img src={logo} alt="" className="logo" />
+        <ul className="icons">
+          <li className="icon">
+            <img src={document} alt="" />
+          </li>
+          <li className="icon">
+            <img src={folder} alt="" />
+          </li>
+          <li className="icon">
+            <img src={upload} alt="" />
+          </li>
+        </ul>
+      </div>
+      <div className="storage-container">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You've used <span>{usedStorage} GB</span> of your storage
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="wrap">
+          <input
+            id="slider"
+            onChange={handleChange}
+            type="range"
+            min={0}
+            max={1000}
+            value={usedStorage}
+          />
+          <div className="track">
+            <div
+              className="innerTrack"
+              style={{ width: `${usedStorage / 10}%` }}
+            ></div>
+          </div>
+          <div
+            className="thumb"
+            style={{
+              left: `${
+                usedStorage / 10 >= 50
+                  ? usedStorage / 10 - 2.5
+                  : usedStorage / 10 + 2.5
+              }%`,
+              transform: `translate(-${usedStorage / 10}%, -50%)`,
+            }}
+          ></div>
+        </div>
+        <div className="range">
+          <span>0 GB</span>
+          <span>1000 GB</span>
+        </div>
+        <div className="remaining">
+          <p>
+            <span>{1000 - usedStorage}</span> GB left
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
